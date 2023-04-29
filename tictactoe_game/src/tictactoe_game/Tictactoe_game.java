df9cd1ad8c4ac6ec6b045e007d287baf8182af2f
berfin-karaagac <131704196+berfin-karaagac@users.noreply.github.com>
2023-04-29 18:45:58 +0300

    java ile tic-tac-toe game

---
+++ tictactoe_game/src/tictactoe_game/Tictactoe_game.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tictactoe_game;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Tictactoe_game {
+
+    public static void main(String[] args) {
+       tic_tac_toe x=new tic_tac_toe();
+    }
+    
+}
